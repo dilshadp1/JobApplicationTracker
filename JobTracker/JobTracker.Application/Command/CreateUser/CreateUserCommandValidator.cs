141dc448e56fa66666f2fd2b7f1df552d72c2e0a
Tinu Clara Emmanuel <tinu.clara@geekywolf.com>
2025-11-26 15:08:37 +0530

    add createusercommand validator

--- JobTracker/JobTracker.Application/Command/CreateUser/CreateUserCommandValidator.cs
+++ JobTracker/JobTracker.Application/Command/CreateUser/CreateUserCommandValidator.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace JobTracker.Application.Command.CreateUser
 {
-    //public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
-    //{
-    //}
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            // 1. First Name Validation
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First Name is required.")
+                .MinimumLength(3).WithMessage("First Name must be at least 3 characters.")
+                .MaximumLength(50).WithMessage("First Name must not exceed 50 characters.");
+
+            // 2. Last Name Validation
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last Name is required.");
+
+            // 3. Email Validation
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("A valid email address is required.");
+
+            // 4. Password Validation
+            RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters.")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches(@"[0-9]").WithMessage("Password must contain at least one number.")
+            .Matches(@"[\W]").WithMessage("Password must contain at least one special character (e.g. ! @ # $).");
+
+            // 5. Phone Validation (Strict)
+            RuleFor(x => x.Phone)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .Matches(@"^\d+$").WithMessage("Phone number must contain only digits.")
+                .Length(10).WithMessage("Phone number must be exactly 10 digits.");
+        }
+    }
 }
