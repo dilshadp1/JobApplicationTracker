ce48094af7789af5a58447ebc84d872bddf483c9
dilshadp1 <dilshad.p@geekywolf.com>
2025-11-26 15:21:12 +0530

    added enum

---
+++ JobTracker/JobTracker.Domain/Enums/InterviewMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.Domain.Enums
+{
+    public enum InterviewMode
+    {
+        online = 0,
+        offline = 1
+    }
+}
