4ae078fe62eb97d0c7162d1d76e9e8174eccbff7
Tinu Clara Emmanuel <tinu.clara@geekywolf.com>
2025-11-26 16:31:30 +0530

    UserRegistration Done

--- JobTracker/JobTracker.API/Program.cs
+++ JobTracker/JobTracker.API/Program.cs
@@ -1,3 +1,11 @@
+using System.Reflection;
+using FluentValidation;
+using JobTracker.Application.Behaviors;
+using JobTracker.Application.Interfaces;
+using JobTracker.Infrastructure.Data;
+using JobTracker.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+try
+{
+    var appAssembly = Assembly.Load("JobTracker.Application");
+
+    builder.Services.AddMediatR(cfg =>
+    {
+        cfg.RegisterServicesFromAssembly(appAssembly);
+        cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    });
+    builder.Services.AddValidatorsFromAssembly(appAssembly);
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("CRITICAL ERROR: Could not find JobTracker.Application.dll");
+}
 
 var app = builder.Build();
 
