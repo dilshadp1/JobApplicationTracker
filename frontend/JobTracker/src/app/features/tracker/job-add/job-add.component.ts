d661ee5f56a1ded0e0e58a9e0da17e1073985c4d
dilshadp1 <dilshad.p@geekywolf.com>
2025-11-27 12:35:50 +0530

    added tracker module frontend and set routes

---
+++ frontend/JobTracker/src/app/features/tracker/job-add/job-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-add',
+  imports: [],
+  templateUrl: './job-add.component.html',
+  styleUrl: './job-add.component.scss'
+})
+export class JobAddComponent {
+
+}
