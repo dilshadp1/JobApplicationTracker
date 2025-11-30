d6765357607b33d4d64c15a0cbf19211e372aa61
dilshadp1 <dilshad.p@geekywolf.com>
2025-11-30 10:51:06 +0530

    adde jobapplicationrepo

---
+++ JobTracker/JobTracker.Application/Interfaces/IJobApplicationRepository.cs
@@ -0,0 +1,14 @@
+﻿using JobTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.Application.Interfaces
+{
+    public interface IJobApplicationRepository : IGenericRepository<JobApplication>
+    {
+        Task<List<JobApplication>> GetApplicationsWithDetailsAsync(int userId);
+    }
+}
---
+++ JobTracker/JobTracker.Infrastructure/Repositories/JobApplicationRepository.cs
@@ -0,0 +1,14 @@
+﻿using JobTracker.Application.Interfaces;
+using JobTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.Infrastructure.Repositories
+{
+    public class JobApplicationRepository(GenericRepository<JobApplication>) : IJobApplicationRepository
+    {
+    }
+}
